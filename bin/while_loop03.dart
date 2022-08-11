7252cf393c8e7cda4d408151e73f293ce84baed1
Ozodbekyum <hello7ozod@gamil.com>
2022-08-11 09:50:04 +0500

    while03

--- bin/while_loop03.dart
+++ bin/while_loop03.dart
@@ -2,4 +2,18 @@
   Create function func with "a " argument
   Return the sum of odd numbers up to "a".
 */
-void main() {}
+int func(int a) {
+  int x = 0;
+  int y = 0;
+  while (x < a) {
+    x += 1;
+    if (x % 2 == 1) {
+      y = y + x;
+    }
+  }
+  return y;
+}
+
+void main() {
+  print(func(15));
+}
