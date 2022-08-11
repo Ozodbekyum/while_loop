2a03924b5fc88ae46a354dccda56ba5e01e67e39
Ozodbekyum <hello7ozod@gamil.com>
2022-08-11 10:05:04 +0500

    while05

--- bin/while_loop05.dart
+++ bin/while_loop05.dart
@@ -2,4 +2,21 @@
   Create function func with "a " argument
   Find the difference between the sum of odd numbers and the sum of even numbers up to a.
 */
-void main() {}
+int func(int a) {
+  int q = 0;
+  int w = 0;
+  int e = 0;
+  while (q < a) {
+    q += 1;
+    if (q % 2 == 1) {
+      w += q;
+    } else if (q % 2 == 0) {
+      e += q;
+    }
+  }
+  return w - e;
+}
+
+void main() {
+  print(func(50));
+}
