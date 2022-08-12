0c2b934cdc3c2a1c6d9aff8f1ffb0538c8a42c24
Ozodbekyum <hello7ozod@gamil.com>
2022-08-12 09:22:40 +0500

    while10

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -6,4 +6,25 @@
   
   
 */
-void main() {}
+String func(int a) {
+  int c = 0;
+  int x = 0;
+
+  while (c <= a) {
+    c += 1;
+    if (a % c == 0) {
+      x += 1;
+    }
+  }
+  String i = '';
+  if (x == 2) {
+    i = 'tub son';
+  } else {
+    i = 'tub son emas';
+  }
+  return i;
+}
+
+void main() {
+  print(func(7));
+}
